cf6bc19d5bd480e8d208baf4701504e7bc33c5b2
247720 Piotr Lewkowicz <247720@edu.p.lodz.pl>
2024-03-05 22:13:29 +0100

    Initial commit

---
+++ Model/src/main/java/pl/kryptografia/aes/AES.java
@@ -0,0 +1,4 @@
+package pl.kryptografia.aes;
+
+public class AES {
+}
---
+++ View/src/main/java/pl/kryptografia/aes/Main.java
@@ -0,0 +1,7 @@
+package pl.kryptografia.aes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
